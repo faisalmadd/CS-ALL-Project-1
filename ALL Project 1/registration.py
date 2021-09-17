37d12a409384d75144080b865c1c670849d2bb4d
faisalmadd <84001348+faisalmadd@users.noreply.github.com>
2021-09-17 08:36:07 +0800

    Add files via upload

---
+++ ALL Project 1/registration.py
@@ -0,0 +1,263 @@
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
+
+f = ('Arial', 14)
+
+con = sqlite3.connect('userdata.db')
+cur = con.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS record(
+                    name text, 
+                    email text, 
+                    contact number, 
+                    gender text, 
+                    country text,
+                    password text
+                )
+            ''')
+con.commit()
+
+ws = Tk()
+ws.title("eBook Reader - Registration Page")
+ws.geometry('800x600')
+ws.config(bg='#F44336')
+
+
+def insert_record():
+    check_counter = 0
+    warn = ""
+    if register_name.get() == "":
+        warn = "Name can't be empty"
+    else:
+        check_counter += 1
+
+    if register_email.get() == "":
+        warn = "Email can't be empty"
+    else:
+        check_counter += 1
+
+    if register_mobile.get() == "":
+        warn = "Contact can't be empty"
+    else:
+        check_counter += 1
+
+    if var.get() == "":
+        warn = "Select Gender"
+    else:
+        check_counter += 1
+
+    if variable.get() == "":
+        warn = "Select Country"
+    else:
+        check_counter += 1
+
+    if register_pwd.get() == "":
+        warn = "Password can't be empty"
+    else:
+        check_counter += 1
+
+    if pwd_again.get() == "":
+        warn = "Re-enter password can't be empty"
+    else:
+        check_counter += 1
+
+    if register_pwd.get() != pwd_again.get():
+        warn = "Passwords didn't match!"
+    else:
+        check_counter += 1
+
+    if check_counter == 8:
+        try:
+            con = sqlite3.connect('userdata.db')
+            cur = con.cursor()
+            cur.execute("INSERT INTO record VALUES (:name, :email, :contact, :gender, :country, :password)", {
+                'name': register_name.get(),
+                'email': register_email.get(),
+                'contact': register_mobile.get(),
+                'gender': var.get(),
+                'country': variable.get(),
+                'password': register_pwd.get()
+
+            })
+            con.commit()
+            messagebox.showinfo('confirmation', 'Record Saved')
+
+        except Exception as ep:
+            messagebox.showerror('', ep)
+    else:
+        messagebox.showerror('Error', warn)
+
+var = StringVar()
+var.set('male')
+
+countries = []
+variable = StringVar()
+world = open('countries.txt', 'r')
+for country in world:
+    country = country.rstrip('\n')
+    countries.append(country)
+variable.set(countries[106])
+
+# widgets
+l = Label(
+    ws,
+    text="Registration",
+    bg='#F44336')
+l.config(font=("Arial", 20))
+
+l2 = Label(
+    ws,
+    text="Create A New Account",
+    bg='#F44336')
+l2.config(font=("Arial", 18))
+
+right_frame = Frame(
+    ws,
+    bd=0,
+    bg='#F44336',
+    relief=SOLID,
+    padx=10,
+    pady=10
+)
+
+Label(
+    right_frame,
+    text="Enter Name",
+    bg='#CCCCCC',
+    font=f
+).grid(row=0, column=0, sticky=W, pady=10)
+
+Label(
+    right_frame,
+    text="Enter Email",
+    bg='#CCCCCC',
+    font=f
+).grid(row=1, column=0, sticky=W, pady=10)
+
+Label(
+    right_frame,
+    text="Contact Number",
+    bg='#CCCCCC',
+    font=f
+).grid(row=2, column=0, sticky=W, pady=10)
+
+Label(
+    right_frame,
+    text="Select Gender",
+    bg='#CCCCCC',
+    font=f
+).grid(row=3, column=0, sticky=W, pady=10)
+
+Label(
+    right_frame,
+    text="Select Country",
+    bg='#CCCCCC',
+    font=f
+).grid(row=4, column=0, sticky=W, pady=10)
+
+Label(
+    right_frame,
+    text="Enter Password",
+    bg='#CCCCCC',
+    font=f
+).grid(row=5, column=0, sticky=W, pady=10)
+
+Label(
+    right_frame,
+    text="Re-Enter Password",
+    bg='#CCCCCC',
+    font=f
+).grid(row=6, column=0, sticky=W, pady=10)
+
+gender_frame = LabelFrame(
+    right_frame,
+    bg='#CCCCCC',
+    padx=10,
+    pady=10,
+)
+
+register_name = Entry(
+    right_frame,
+    font=f
+)
+
+register_email = Entry(
+    right_frame,
+    font=f
+)
+
+register_mobile = Entry(
+    right_frame,
+    font=f
+)
+
+male_rb = Radiobutton(
+    gender_frame,
+    text='Male',
+    bg='#CCCCCC',
+    variable=var,
+    value='male',
+    font=('Times', 10),
+
+)
+
+female_rb = Radiobutton(
+    gender_frame,
+    text='Female',
+    bg='#CCCCCC',
+    variable=var,
+    value='female',
+    font=('Times', 10),
+
+)
+
+register_country = OptionMenu(
+    right_frame,
+    variable,
+    *countries)
+
+register_country.config(
+    width=15,
+    font=('Times', 12)
+)
+register_pwd = Entry(
+    right_frame,
+    font=f,
+    show='*'
+)
+pwd_again = Entry(
+    right_frame,
+    font=f,
+    show='*'
+)
+
+register_btn = Button(
+    right_frame,
+    width=15,
+    text='Join Now!',
+    bg='green',
+    font=f,
+    relief=SOLID,
+    cursor='hand2',
+    command=insert_record
+)
+
+register_name.grid(row=0, column=1, pady=10, padx=20)
+register_email.grid(row=1, column=1, pady=10, padx=20)
+register_mobile.grid(row=2, column=1, pady=10, padx=20)
+register_country.grid(row=4, column=1, pady=10, padx=20)
+register_pwd.grid(row=5, column=1, pady=10, padx=20)
+pwd_again.grid(row=6, column=1, pady=10, padx=20)
+register_btn.grid(row=7, column=1, pady=10, padx=20)
+l.grid(row=0, column=0, pady=10, padx=20)
+l2.grid(row=0, column=0, pady=10, padx=20)
+l.place(x=355, y=20)
+l2.place(x=315, y=55)
+right_frame.place(x=220, y=120)
+
+gender_frame.grid(row=3, column=1, pady=10, padx=20)
+male_rb.pack(expand=True, side=LEFT)
+female_rb.pack(expand=True, side=LEFT)
+
+# infinite loop
+ws.mainloop()
