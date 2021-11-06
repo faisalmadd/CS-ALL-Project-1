0ffd71d7ae84951acb5af85b0ac936b193bf3e03
faisalmadd <84001348+faisalmadd@users.noreply.github.com>
2021-11-07 03:47:17 +0800

    updated code

--- ALL Project 1/ebook_app.py
+++ ALL Project 1/ebook_app.py
@@ -6,62 +6,12 @@ from tkinter import SOLID, N, messagebox, W, LEFT, RIDGE, BOTH, RIGHT, TOP, file
 from PIL import Image, ImageTk
 from pdf2image import convert_from_path
 from chatclient import Client
+from db_handle import *
 import random
 
 f = ('Arial', 14)
 HOST = '127.0.0.1'
 PORT = 8080
-
-
-def readCoverFile(bookID):
-    con = sqlite3.connect('userdata.db')
-    cursor = con.cursor()
-    query = "SELECT * from books where book_id = ?"
-    cursor.execute(query, (bookID,))
-    bookRecord = cursor.fetchone()
-    coverFile = bookRecord[7]
-    return coverFile
-
-
-def readBookTitle(bookID):
-    con = sqlite3.connect('userdata.db')
-    cursor = con.cursor()
-    query = "SELECT * from books where book_id = ?"
-    cursor.execute(query, (bookID,))
-    bookRecord = cursor.fetchone()
-    bookTitle = bookRecord[1]
-    return bookTitle
-
-
-def readBookAuthor(bookID):
-    con = sqlite3.connect('userdata.db')
-    cursor = con.cursor()
-    query = "SELECT * from books where book_id = ?"
-    cursor.execute(query, (bookID,))
-    bookRecord = cursor.fetchone()
-    bookAuthor = bookRecord[2]
-    return bookAuthor
-
-
-def readBookSynopsis(bookID):
-    con = sqlite3.connect('userdata.db')
-    cursor = con.cursor()
-    query = "SELECT * from books where book_id = ?"
-    cursor.execute(query, (bookID,))
-    bookRecord = cursor.fetchone()
-    bookSynopsis = bookRecord[4]
-    return bookSynopsis
-
-
-def readBookPath(bookID):
-    con = sqlite3.connect('userdata.db')
-    cursor = con.cursor()
-    query = "SELECT * from books where book_id = ?"
-    cursor.execute(query, (bookID,))
-    bookRecord = cursor.fetchone()
-    bookPath = bookRecord[6]
-    return bookPath
-
 
 coverList = []
 titleList = []
